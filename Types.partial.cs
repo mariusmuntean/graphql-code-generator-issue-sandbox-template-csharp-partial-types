2af360983b10cdf914184b15450a42d6a1cffe77
Marius Muntean <marius.muntean@live.com>
2024-07-29 22:32:35 +0200

    add partial types

---
+++ Types.partial.cs
@@ -0,0 +1,9 @@
+namespace GraphQLCodeGen
+
+public partial Types
+{
+    public partial class FirstOrSecondUnionType {}
+
+    public partial class FirstUnionType : FirstOrSecondUnionType
+    public partial class SecondUnionType : FirstOrSecondUnionType
+}
